ec5ed919d41831dce766ffe6f7df4285c5d7fc2a
ersousa <ersousa84@gmail.com>
2023-06-14 23:44:00 -0300

    add swagger apiInfo config

--- src/main/java/com/pokemon/pokemonapi/configuration/SwaggerConfiguration.java
+++ src/main/java/com/pokemon/pokemonapi/configuration/SwaggerConfiguration.java
@@ -1,20 +1,41 @@
 package com.pokemon.pokemonapi.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
+
+    @Autowired
+    private BuildProperties buildProperties;
+
+
     @Bean
     public Docket docket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(buildProperties.getArtifact() + " API")
+                .description(buildProperties.getArtifact() + " - API Swagger documentation")
+                .version(buildProperties.getVersion())
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
     }
+
 }
